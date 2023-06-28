286da39c1dba24dd3c7a39e77b66a7272efd9737
Ariel070797 <136457636+Ariel070797@users.noreply.github.com>
2023-06-28 09:37:00 +0200

    Create Note.java
    Clase para crear las notas

---
+++ AndroidStudio/AppNotas/main/Note.java
@@ -0,0 +1,36 @@
+public class Note {
+    protected int id;
+    protected String title;
+    protected int priority;
+
+    public Note (int id, String title, int priority){
+        this.id=id;
+        this.title=title;
+        this.priority=priority;
+    }
+
+    public int getId(){
+
+        return this.id;
+    }
+    public void setId(int id){
+
+        this.id=id;
+    }
+    public String getTitle(){
+
+        return this.title;
+    }
+    public void setTitle(String title){
+
+        this.title=title;
+    }
+    public int getPriority(){
+
+        return this.priority;
+    }
+    public void setPriority(int priority){
+
+        this.priority=priority;
+    }
+}
